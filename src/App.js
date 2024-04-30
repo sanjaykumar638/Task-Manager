4066cf73f126482746826a58ddd7a8332e5ab8b3
sanjaykumar638 <sk7067194@gmail.com>
2024-04-30 22:22:17 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing Components
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+// Importing React Hooks
+import { useState, useEffect } from 'react';
+// Importing Packages
+import { v4 as uuidv4 } from 'uuid';
+import Swal from "sweetalert2";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // All States
+    const [loading, setloading] = useState(true); // Pre-loader before page renders
+    const [tasks, setTasks] = useState([]); // Task State
+    const [showAddTask, setShowAddTask] = useState(false); // To reveal add task form
+
+    // Pre-loader
+    useEffect(() => {
+        setTimeout(() => {
+            setloading(false);
+        }, 3500);
+    }, [])
+
+    // Fetching from Local Storage
+    const getTasks = JSON.parse(localStorage.getItem("taskAdded"));
+
+    useEffect(() => {
+        if (getTasks == null) {
+            setTasks([])
+        } else {
+            setTasks(getTasks);
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    // Add Task
+    const addTask = (task) => {
+        const id = uuidv4();
+        const newTask = { id, ...task }
+
+        setTasks([...tasks, newTask]);
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Yay...',
+            text: 'You have successfully added a new task!'
+        })
+
+        localStorage.setItem("taskAdded", JSON.stringify([...tasks, newTask]));
+    }
+
+    // Delete Task
+    const deleteTask = (id) => {
+        const deleteTask = tasks.filter((task) => task.id !== id);
+
+        setTasks(deleteTask);
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Oops...',
+            text: 'You have successfully deleted a task!'
+        })
+
+        localStorage.setItem("taskAdded", JSON.stringify(deleteTask));
+    }
+
+    // Edit Task
+    const editTask = (id) => {
+
+        const text = prompt("Task Name");
+        const day = prompt("Day and Time");
+        let data = JSON.parse(localStorage.getItem('taskAdded'));
+
+        const myData = data.map(x => {
+            if (x.id === id) {
+                return {
+                    ...x,
+                    text: text,
+                    day: day,
+                    id: uuidv4()
+                }
+            }
+            return x;
+        })
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Yay...',
+            text: 'You have successfully edited an existing task!'
+        })
+
+        localStorage.setItem("taskAdded", JSON.stringify(myData));
+        window.location.reload();
+    }
+
+    return (
+        <>
+            {
+                loading
+                    ?
+                    <div className="spinnerContainer">
+                        <div className="spinner-grow text-primary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-secondary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-success" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-danger" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    :
+                    <div className="container">
+                        {/* App Header that has open and App Name */}
+                        <Header showForm={() => setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
+
+                        {/* Revealing of Add Task Form */}
+                        {showAddTask && <AddTask onSave={addTask} />}
+
+                        {/* Task Counter */}
+                        <h3>Number of Tasks: {tasks.length}</h3>
+
+                        {/* Displaying of Tasks */}
+                        {
+                            tasks.length > 0
+                                ?
+                                (<Tasks tasks={tasks} onDelete={deleteTask} onEdit={editTask} />)
+                                :
+                                ('No Task Found!')
+                        }
+                    </div>
+            }
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ color, text, onClick }) => {
+  return <button onClick={onClick} style={{ backgroundColor: color }} className="btn">{text}</button>
+}
+
+export default Button
